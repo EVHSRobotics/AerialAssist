083d79d960582d242a74d09ceb43a8b3c1ea4a4d
Kevin Wang <wangkevin448@gmail.com>
2014-01-18 12:24:01 -0800

    test vision code

--- Mecanum2014/src/edu/wpi/first/wpilibj/templates/commands/CommandBase.java
+++ Mecanum2014/src/edu/wpi/first/wpilibj/templates/commands/CommandBase.java
@@ -1,10 +1,9 @@
 package edu.wpi.first.wpilibj.templates.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.templates.OI;
-import edu.wpi.first.wpilibj.templates.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.templates.subsystems.VisionCamera;
 
 /**
  * The base for all commands. All atomic commands should subclass CommandBase.
@@ -17,7 +16,7 @@ public abstract class CommandBase extends Command {
     public static OI oi;
     // Create a single static instance of all of your subsystems
   public static DriveTrain driveTrain = new DriveTrain();
-
+  public static VisionCamera visionCamera = new VisionCamera();
     public static void init() {
         // This MUST be here. If the OI creates Commands (which it very likely
         // will), constructing it during the construction of CommandBase (from
---
+++ Mecanum2014/src/edu/wpi/first/wpilibj/templates/subsystems/VisionCamera.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.image.CriteriaCollection;
+import edu.wpi.first.wpilibj.image.NIVision.MeasurementType;
+import edu.wpi.first.wpilibj.templates.commands.Vision;
+
+/**
+ *
+ * @author kevin
+ */
+public class VisionCamera extends Subsystem {
+    
+    public AxisCamera axisCamera;
+    public CriteriaCollection cc; 
+    
+    public VisionCamera() {
+        cc = new CriteriaCollection();      // create the criteria for the particle filter
+        //axisCamera = AxisCamera.getInstance();
+    }
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new Vision());
+    }
+}
