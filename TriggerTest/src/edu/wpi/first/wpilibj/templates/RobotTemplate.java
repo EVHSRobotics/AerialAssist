9d4434dab93c1dc593a5ed06d75a3776f5d69511
Kevin Wang <wangkevin448@gmail.com>
2014-03-05 11:42:26 -0800

    hopefully our trigger works

---
+++ TriggerTest/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Victor;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SimpleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class RobotTemplate extends SimpleRobot {
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     */
+    public void autonomous() {
+        
+    }
+
+    /**
+     * This function is called once each time the robot enters operator control.
+     */
+    public void operatorControl() {
+        Victor windowMotor = new Victor(4);
+        //A 1 B 2 
+        Joystick controller = new Joystick(1);
+        boolean buttonA = controller.getRawButton(1);
+        boolean buttonB = controller.getRawButton(2);
+        while(true){
+            Timer.delay(0.05);
+            if(buttonA) {
+                windowMotor.set(0.6);
+            } else if (buttonB) {
+                windowMotor.set(-0.6);
+            } else {
+                windowMotor.set(0);
+            }
+        }
+    }
+    
+    /**
+     * This function is called once each time the robot enters test mode.
+     */
+    public void test() {
+    
+    }
+}
